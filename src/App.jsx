6bae90095e3895710e1075974e69d2a1c5a126c5
valentin <valentinc.boury@gmail.com>
2023-08-04 18:39:10 +0200

    Ajout trad navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,44 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import TheHeader from './components/Header/TheHeader';
 import Router from './Router';
+import i18next from 'i18next';
+import { initReactI18next } from'react-i18next';
+
+i18next.use(initReactI18next)
+  .init({ 
+    resources: {
+      fr: { 
+        translation: {
+          home: 'Accueil' ,
+          about: 'A Propos',
+          project: 'Projets',
+          paiement: 'Paiement',
+          countries: 'Pays',
+          pokedex: 'Pokedex',
+          contact: 'Contacter',
+          users: 'Utilisateurs',
+          connexion: 'Connexion',
+          registration: 'Création'
+        }  
+      } ,
+      en: { 
+        translation: {
+          home: 'Home' ,
+          about: 'About',
+          project: 'Project',
+          paiement: 'Paiement',
+          countries: 'Countries',
+          pokedex: 'Pokedex',
+          contact: 'Contact',
+          users: 'Users',
+          connexion: 'Connection',
+          registration: 'Registration'
+        }
+      }
+    } ,
+    lng: 'fr',
+    fallbackLng: 'fr'
+  })
 
 const App = () => {
   return (
--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -1,28 +1,45 @@
 import "./style/Navigation.css";
 import logo from "../../img/logo.jpeg";
 import { NavLink } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const Navigation = () => {
+    const { t, i18n } = useTranslation();
+
+    function handleLangChange(event) {
+        console.log(event.target.value);
+        i18n.changeLanguage(event.target.value);
+    }
+
     return (
         <div className="navigation">
             <nav>
-                <img src={logo} alt="Logo" />
+                <NavLink to="/">
+                    <img src={logo} alt="Logo" />
+                </NavLink>
                 <ul>
-                    <li><NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")} >Home</NavLink></li>
-                    <li><NavLink to="/about" className={(nav) => (nav.isActive ? "nav-active" : "")} >About</NavLink></li>
-                    <li><NavLink to="/project" className={(nav) => (nav.isActive ? "nav-active" : "")} >Project</NavLink></li>
-                    <li><NavLink to="/paiement" className={(nav) => (nav.isActive ? "nav-active" : "")}>Paiement</NavLink></li>
-                    <li><NavLink to="/countries" className={(nav) => (nav.isActive ? "nav-active" : "")}>Countries</NavLink></li>
-                    <li><NavLink to="/pokedex" className={(nav) => (nav.isActive ? "nav-active" : "")} >Pokedex</NavLink></li>
-                    <li><NavLink to="/contact" className={(nav) => (nav.isActive ? "nav-active" : "")} >Contact Us</NavLink></li>
-                    <li><NavLink to="/users" className={(nav) => (nav.isActive ? "nav-active" : "")} >Users Contact</NavLink></li>
-                    <li><NavLink to="/connexion" className={(nav) => (nav.isActive ? "nav-active" : "")}>Connexion</NavLink></li>
-                    <li><NavLink to="/registration" className={(nav) => (nav.isActive ? "nav-active" : "")}>Registration</NavLink></li>
+                    <li><NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")} >{t('home')}</NavLink></li>
+                    <li><NavLink to="/about" className={(nav) => (nav.isActive ? "nav-active" : "")} >{t('about')}</NavLink></li>
+                    <li><NavLink to="/project" className={(nav) => (nav.isActive ? "nav-active" : "")} >{t('project')}</NavLink></li>
+                    <li><NavLink to="/paiement" className={(nav) => (nav.isActive ? "nav-active" : "")}>{t('paiement')}</NavLink></li>
+                    <li><NavLink to="/countries" className={(nav) => (nav.isActive ? "nav-active" : "")}>{t('countries')}</NavLink></li>
+                    <li><NavLink to="/pokedex" className={(nav) => (nav.isActive ? "nav-active" : "")} >{t('pokedex')}</NavLink></li>
+                    <li><NavLink to="/contact" className={(nav) => (nav.isActive ? "nav-active" : "")} >{t('contact')}</NavLink></li>
+                    <li><NavLink to="/users" className={(nav) => (nav.isActive ? "nav-active" : "")} >{t('users')}</NavLink></li>
+                    <li><NavLink to="/connexion" className={(nav) => (nav.isActive ? "nav-active" : "")}>{t('connexion')}</NavLink></li>
+                    <li><NavLink to="/registration" className={(nav) => (nav.isActive ? "nav-active" : "")}>{t('registration')}</NavLink></li>
+                    <div>
+                        <select onChange={handleLangChange} defaultValue="fr">
+                            <option value="fr">FR</option>
+                            <option value="en">EN</option>
+                        </select>
+                    </div>
                     <div className="cursor"></div>
                 </ul>
             </nav>
         </div>
     );
 }
+
 
 export default Navigation;
